79216170439b22e218d36ffa6c7cf04bec96a4c9
EduardJhonnHerreraSanchez <eduardjhonnherrera@gmail.com>
2021-09-15 19:46:58 -0300

    Clase 5: Score, Restart, message

--- nlc_dino_runner/Componentes/game.py
+++ nlc_dino_runner/Componentes/game.py
@@ -1,9 +1,11 @@
 import pygame
 
+from nlc_dino_runner.Componentes import text_utils
 from nlc_dino_runner.Componentes.Obstacles.cactus import Cactus
 from nlc_dino_runner.Componentes.Obstacles.obstaclesManager import ObstaclesManager
 from nlc_dino_runner.Componentes.dinosaur import Dinosaur
-from nlc_dino_runner.utils.constants import TITLE, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, BG, FPS, SMALL_CACTUS, LARGE_CACTUS
+from nlc_dino_runner.utils.constants import TITLE, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, BG, FPS, SMALL_CACTUS, \
+    LARGE_CACTUS, RUNNING
 
 
 class Game:
@@ -21,17 +23,22 @@ class Game:
         self.game_speed = 20
         self.player = Dinosaur()
         self.obstacle_manager = ObstaclesManager()
+        self.points = 0
+        self.running = True
+        self.death_count = 0
         # self.cactusSmall = Cactus(SMALL_CACTUS)
         # self.cactusLarge = Cactus(LARGE_CACTUS)
 
     def run(self):
         print("JUGAR")
+        self.obstacle_manager.reset_obstacles()
+        self.points = 0
         self.playing = True
         while self.playing:
             self.event()
             self.update()
             self.draw()
-        pygame.quit()
+        # pygame.quit()
 
     def event(self):
         for event in pygame.event.get():
@@ -44,14 +51,23 @@ class Game:
         self.obstacle_manager.update(self)
 
     def draw(self):
+
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
+        self.score()
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
 
         pygame.display.update()
         pygame.display.flip()  # Actualización de toda la ventana
+
+    def score(self):
+        self.points += 1
+        if self.points % 150 == 0:
+            self.game_speed += 1
+        score_element,score_element_rect  = text_utils.get_score_element(self.points)
+        self.screen.blit(score_element,score_element_rect)
 
     def draw_background(self):
         image_width = BG.get_width()
@@ -64,4 +80,45 @@ class Game:
             self.x_pos_bg = 0
 
         self.x_pos_bg -= self.game_speed
+
+    def execute(self):
+        while self.running: # Running es para saber si el juego esta ejecutandose
+            if not self.playing:
+                self.show_menu()
+
+    def show_menu(self):
+        self.running = True
+
+        white_color = (255,255,255)
+        self.screen.fill(white_color)
+
+        # mostrar el menu
+        self.print_menu_elements()
+        pygame.display.update()
+        self.handle_key_events_on_menu()
+
+    def handle_key_events_on_menu(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                self.playing = False
+                pygame.display.quit()
+                pygame.quit()
+                exit()
+
+            if event.type == pygame.KEYDOWN:
+                self.run()
+
+    def print_menu_elements(self):
+        half_screen_height = SCREEN_HEIGHT // 2
+
+        text, text_rect = text_utils.get_centered_message("Press any key to restart your score")
+        self.screen.blit(text, text_rect)
+
+        death_score,death_score_rect = text_utils.get_centered_message("Death count is: " + str(self.death_count), height=SCREEN_HEIGHT + 50)
+        self.screen.blit(death_score, death_score_rect)
+
+        # Imprimiendo dinosaurio de portada
+        self.screen.blit(RUNNING[0], ((SCREEN_WIDTH // 2) - 40, half_screen_height - 150))
+
 
--- nlc_dino_runner/main.py
+++ nlc_dino_runner/main.py
@@ -3,5 +3,5 @@ from nlc_dino_runner.Componentes.game import Game
 
 if __name__ == "__main__":
     game = Game()
-    game.run()
+    game.execute()
 
